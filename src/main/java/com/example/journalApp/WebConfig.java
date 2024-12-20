59f27e11d9d6dd183bef943d53bb6be6f4b0b2ec
Sparsh garg <95720854+Sparshgarg14@users.noreply.github.com>
2024-12-21 01:54:22 +0530

    Update WebConfig.java

--- src/main/java/com/example/journalApp/WebConfig.java
+++ src/main/java/com/example/journalApp/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://portfolio-full-stack-five.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
